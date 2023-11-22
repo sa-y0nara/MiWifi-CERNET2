1ae95508cc04c5e7f4ff564a629d72c35f00d2b2
SAYONARA <149249833+sa-y0nara@users.noreply.github.com>
2023-11-22 15:27:40 +0800

    Add files via upload

---
+++ config.mk
@@ -0,0 +1,38 @@
+#### Choose/Add your modules here ####
+PLUGIN_MODULES := \
+	packet_plugin_printer \
+	packet_plugin_rjv3
+
+# Linux
+PLUGIN_MODULES += if_impl_sockraw
+
+# macOS / BSD
+# PLUGIN_MODULES += if_impl_bpf
+
+# Other OS
+# PLUGIN_MODULES += if_impl_libpcap
+
+# This implementation is not included in the code.
+# If you need it, add it yourselves.
+# PLUGIN_MODULES += ifaddrs
+
+ENABLE_DEBUG  := false
+ENABLE_ICONV  := false
+ENABLE_GBCONV := true
+STATIC_BUILD  := true
+
+# If your platform has iconv_* integrated into libc, change to false
+# Affects dynamic linking
+LIBICONV_STANDALONE := false
+
+CUSTOM_CFLAGS :=
+CUSTOM_LDFLAGS :=
+CUSTOM_LIBS :=
+
+# Example for cross-compiling
+CC := aarch64-openwrt-linux-gcc
+# ENABLE_ICONV := true
+# CUSTOM_CFLAGS += -I/home/me/libiconv-1.14/include
+# CUSTOM_LIBS += /home/me/arm/libiconv.a
+# PLUGIN_MODULES += ifaddrs
+# STATIC_BUILD := true
